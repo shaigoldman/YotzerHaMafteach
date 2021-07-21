c143512e8b0c2a57f6fe89936f00eda01944973b
shai goldman <shaitzvi@gmail.com>
2021-07-20 22:01:16 -0400

    initial import of code

---
+++ LoadSamples.py
@@ -0,0 +1,8 @@
+cwd = '/'.join(__file__.split('/')[:-1])
+
+def load_sample(i):
+    path = cwd + f'/Sample{i}.txt'
+    
+    with open(path) as f:
+        text = f.read().split('\n')
+    return text
